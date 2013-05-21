03a48e4932dc05933567b1022d6808fa96db62a4
rschellhorn <rob.schellhorn@finalist.nl>
2013-05-21 15:47:12 +0200

    Upgrade dependencies

--- project/Build.scala
+++ project/Build.scala
@@ -8,9 +8,9 @@ object ApplicationBuild extends Build {
     val appVersion = "1.0-SNAPSHOT"
 
     val appDependencies = Seq(
-        "org.webjars" % "webjars-play" % "2.1.0-1",
-        "org.webjars" % "angular-strap" % "0.7.2-1",
-        "org.webjars" % "highcharts" % "3.0.1"
+        "org.webjars" % "angular-strap" % "0.7.3",
+        "org.webjars" % "highcharts" % "3.0.1",
+        "org.webjars" % "webjars-play" % "2.1.0-1"
     )
 
     val main = play.Project(appName, appVersion, appDependencies).settings()
