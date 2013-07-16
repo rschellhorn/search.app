5dd76aecf8578d0df77f767ed85ef1eb7a19779b
rschellhorn <rob.schellhorn@finalist.nl>
2013-07-16 15:02:41 +0200

    Upgrade libs

--- project/Build.scala
+++ project/Build.scala
@@ -8,8 +8,8 @@ object ApplicationBuild extends Build {
     val appVersion = "1.0-SNAPSHOT"
 
     val appDependencies = Seq(
-        "com.googlecode.ez-vcard" % "ez-vcard" % "0.8.1",
-        "org.webjars" % "angular-strap" % "0.7.3",
+        "com.googlecode.ez-vcard" % "ez-vcard" % "0.8.2",
+        "org.webjars" % "angular-strap" % "0.7.4",
         "org.webjars" % "highcharts" % "3.0.1",
         "org.webjars" %% "webjars-play" % "2.1.0-2"
     )
